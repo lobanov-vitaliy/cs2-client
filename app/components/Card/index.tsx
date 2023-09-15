4e01dfaad6ce57ac555693293e323b2b716e2b84
Beast51 <7007500@gmail.com>
2023-09-15 17:18:35 +0300

    move example of card to all-matches

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { FC } from 'react';
 import { BodyProps, CardProps, FooterProps, HeaderProps } from './card.type';
 import cn from 'classnames';
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-'use client';
 import Button from './components/Button';
-import Card from './components/Card';
 
 export default function HomePage() {
   return (
@@ -12,32 +10,6 @@ export default function HomePage() {
       <Button variant="outline">Button</Button>
       <Button variant="soft">Button</Button>
       <Button variant="ghost">Button</Button>
-
-      <div className="pt-4"></div>
-      <Card>
-        <Card.Header
-          className="header-class"
-          onClose={() => console.log('click')}
-          title="Good card"
-        >
-          header text
-        </Card.Header>
-        <Card.Body>
-          <h6 className="card-title">What planning process needs ?</h6>
-          <p className="card-text text-muted mb-0">
-            Intrinsically incubate intuitive opportunities and real-time
-            potentialities for change for interoperable meta-services itself or
-            distract the viewer's attention from the layout.
-          </p>
-        </Card.Body>
-        <Card.Footer>
-          <a className="link-success float-end cursor-pointer">
-            Payment Now
-            <i className="ri-arrow-right-s-line align-middle ms-1 lh-1"></i>
-          </a>
-          <p className="text-muted mb-0">5 days Left</p>
-        </Card.Footer>
-      </Card>
     </div>
   );
 }
